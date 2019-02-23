b6612841233d6d3a6c6631acdf26a4fb2e211165
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-22 22:23:46 -0500

    full comment on db

--- connection.js
+++ connection.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
-let mySQL = require("mysql");
-let sqlKeys = require("./keys");
-let connection;
-console.log(sqlKeys.sqlKeys)
-if (process.env.JAWSDB_URL) {
-     connection = mySQL.createConnection(process.env.JAWSDB_URL);
-} else {
-     connection =  mySQL.createConnection(sqlKeys.sqlKeys);
-}
+// require("dotenv").config();
+// let mySQL = require("mysql");
+// let sqlKeys = require("./keys");
+// let connection;
+// console.log(sqlKeys.sqlKeys)
+// if (process.env.JAWSDB_URL) {
+//      connection = mySQL.createConnection(process.env.JAWSDB_URL);
+// } else {
+//      connection =  mySQL.createConnection(sqlKeys.sqlKeys);
+// }
 
-connection.connect(function (err) {
-     if (err) {
-          console.error("error connecting: " + err.stack);
-          return;
-     }
-     console.log("connected as id " + connection.threadId);
-});
-connection.query("SELECT * FROM teams", function(err, data){
-     console.log(data)
-})
-module.exports = connection;
+// connection.connect(function (err) {
+//      if (err) {
+//           console.error("error connecting: " + err.stack);
+//           return;
+//      }
+//      console.log("connected as id " + connection.threadId);
+// });
+// connection.query("SELECT * FROM teams", function(err, data){
+//      console.log(data)
+// })
+// module.exports = connection;
--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-exports.sqlKeys = {
-     host: process.env.HOST,
-     port: process.env.sqlPort,
-     user: process.env.sqlUser,
-     password: process.env.sqlPass,
-     database: process.env.sqlDB
-}
+// exports.sqlKeys = {
+//      host: process.env.HOST,
+//      port: process.env.sqlPort,
+//      user: process.env.sqlUser,
+//      password: process.env.sqlPass,
+//      database: process.env.sqlDB
+// }
 
