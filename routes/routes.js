10c1de2d93d16efd1652cd208222fa8fa2e88af4
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-22 21:55:41 -0500

    Changed Title

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const path = require("path");
 let router = express.Router();
 
 router.get("/", function(req, res){
-     res.render("index", {title:"NHL Site"});
+     res.render("index", {title:"NHL Team Site"});
 })
 
 module.exports = router;
