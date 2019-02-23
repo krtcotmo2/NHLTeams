99793755c98be1fe8ef602b4460b8363b161d51f
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-22 22:52:10 -0500

    Fixed db connection pathing

--- connection.js
+++ connection.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 let mySQL = require("mysql");
 let sqlKeys = require("./keys");
 let connection;
-console.log(sqlKeys.sqlKeys)
 if (process.env.JAWSDB_URL) {
      connection = mySQL.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -16,7 +15,5 @@ connection.connect(function (err) {
      }
      console.log("connected as id " + connection.threadId);
 });
-connection.query("SELECT * FROM teams", function(err, data){
-     console.log(data)
-})
+
 module.exports = connection;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,22 @@
 let express = require('express');
 const path = require("path");
-
+connection = require("../connection");
 let router = express.Router();
 
 router.get("/", function(req, res){
-     res.render("index", {title:"NHL Team Site"});
+     connection.query("SELECT * FROM teams", function(err, data){
+          if(err){
+               res.status(404).statusCode("No Data found");
+          }else{
+               let eastTeams = data.filter( o => {return o.confID != 1});
+               let westTeams = data.filter( o => {return o.confID == 1});
+               res.render("index", {title:"NHL Team Site", eastTeams:eastTeams, westTeams:westTeams});
+          }
+          
+     })
+
+
+     
 })
 
 router.get("/team/:id", function(req, res){
