a52787042423c47108246096a29420a12b8b1118
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-22 22:01:07 -0500

    add team route

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,9 @@ router.get("/", function(req, res){
      res.render("index", {title:"NHL Team Site"});
 })
 
+router.get("/team/:id", function(req, res){
+     console.log(req.params.id);
+     res.render("team")
+
+})
 module.exports = router;
